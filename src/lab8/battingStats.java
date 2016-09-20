b514461fe79480b77a6fc667b8c65048a9e3e459
David Smiley <DGSmiley77@gmail.com>
2016-09-20 17:24:16 -0400

    first commit

---
+++ src/lab8/battingStats.java
@@ -0,0 +1,13 @@
+package lab8;
+
+import java.util.Scanner;
+
+public class battingStats {
+
+	public static void main(String[] args) {
+		// scanner and header
+Scanner input = new Scanner(System.in);
+System.out.println("Welcome to Batting Average Calculator! ");
+	}
+
+}
